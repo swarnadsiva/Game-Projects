f26160df4e35527355a5e78138362c4ffaac0251
DESKTOP-DURGA\Durga <swarnadsiva@gmail.com>
2019-11-05 23:40:53 -0800

    added basic AI movement

---
+++ Movement/Assets/Scripts/Movement/AIMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+
+    protected NavMeshAgent agent;
+    protected GameObject floor;
+    protected float floorSize;
+    protected bool useAIMovement = true; // default will need to be changed in inheriting classes
+
+    Vector3 destination;
+    float distToDest;
+    public float minDistance = 15f;
+
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        floor = GameObject.FindGameObjectWithTag("Floor");
+        floorSize = (floor.transform.localScale.x * 10) / 2; // get one of the scale values (since this is going to be a square
+        // and multiply by 10 since that's the grid value
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (useAIMovement)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                StartCoroutine(GotoNextPoint());
+            }
+        }
+    }
+
+    IEnumerator GotoNextPoint()
+    {
+        do
+        {
+            // find a random point on the floor
+            float x = Random.Range(-floorSize, floorSize);
+            float z = Random.Range(-floorSize, floorSize);
+
+            // check that this point is more than 2f away
+            destination = new Vector3(x, 0, z);
+
+            distToDest = Vector3.Distance(transform.position, destination);
+
+            Debug.LogFormat("({0}, 0, {1}) distance from current point: {2}", x, z, distToDest);
+
+        } while (distToDest < minDistance);
+
+        agent.destination = destination;
+
+        yield return null;
+    }
+}
---
+++ Movement/Assets/Scripts/Movement/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMovement : AIMovement
+{
+    
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        useAIMovement = true;
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
